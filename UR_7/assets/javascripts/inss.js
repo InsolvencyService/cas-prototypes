b958a71ec3932f93d43da427ad6acaa937e1da3e
Z Ali <zali@Zs-MacBook-Pro.local>
2016-01-09 21:30:44 +0000

    Tweak to control name and value

--- UR_7/assets/javascripts/inss.js
+++ UR_7/assets/javascripts/inss.js
@@ -1,1 +1,21 @@
 // JS for CAS
+$('form').on('submit', function(e){
+    e.preventDefault();
+    $('.validate').each(function(){
+        
+        validations = $(this).attr('data-validation');
+        validations = validations.split(" ");
+        
+        for(i=0; i<validations.length; i++){
+            switch(validations[i]){
+                case 'required':
+                    console.log('im required');
+                    break;
+                    
+                case 'email':
+                    console.log('email is invalid');
+                    break;
+            }
+        }
+    });
+});
