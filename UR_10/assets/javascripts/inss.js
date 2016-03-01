2d1339d37814dea8fa5b77f687fc930ef6c5b8f9
Z Ali <zali@Zs-MacBook-Pro.local>
2016-03-01 10:41:25 +0000

    Case preview tweaks

--- UR_10/assets/javascripts/inss.js
+++ UR_10/assets/javascripts/inss.js
@@ -41,11 +41,13 @@ $(function() {
            var scrollTop = $(window).scrollTop(),
                offset = $(this).offset(),
                stickyTitle = $(this).find('.sticky-title');
+           
 
-            if ((scrollTop > offset.top) && (scrollTop < offset.top + $(this).height()))
-               stickyTitle.removeClass('invisible');
-            else
-               stickyTitle.addClass('invisible');
+            if ((scrollTop > offset.top) && (scrollTop < offset.top + ($(this).height()-stickyTitle.height()))){
+               stickyTitle.removeClass('invisible').prev('h2').addClass('blend-with-bg');
+            } else {
+               stickyTitle.addClass('invisible').prev('h2').removeClass('blend-with-bg');
+            }
 
        });
     });
